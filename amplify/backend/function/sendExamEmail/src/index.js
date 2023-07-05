cd0c0adc49bf7b2eb7904199a796de1b22b7e095
ThuHaNgo11 <21520131@gm.uit.edu.vn>
2023-07-05 12:48:57 +0700

    Clean up code

--- amplify/backend/function/sendExamEmail/src/index.js
+++ amplify/backend/function/sendExamEmail/src/index.js
@@ -1,5 +1,6 @@
-import { SendEmailCommand } from "@aws-sdk/client-ses";
-import { sesClient } from "./lib/sesClient.js";
+import {SendEmailCommand} from "@aws-sdk/client-ses";
+import {sesClient} from "./lib/sesClient.js";
+
 /**
  * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
  */
@@ -31,34 +32,34 @@ const createSendEmailCommand = (toAddress, fromAddress, body, subject) => {
 
 
 export const handler = async (event) => {
-  console.log(`EVENT: ${JSON.stringify(event)}`);
-  for (const record of event.Records) {
-    console.log(record.eventID);
-    console.log(record.eventName);
-    console.log('DynamoDB Record: %j', record.dynamodb);
+    console.log(`EVENT: ${JSON.stringify(event)}`);
+    for (const record of event.Records) {
+        console.log(record.eventID);
+        console.log(record.eventName);
+        console.log('DynamoDB Record: %j', record.dynamodb);
 
-    //pull off items from stream
-    const examLink = record.dynamodb.NewImage.examLink.S
-    const toAddress = record.dynamodb.NewImage.toAddress.S
-    const fromAddress = record.dynamodb.NewImage.fromAddress.S
-    const subject = record.dynamodb.NewImage.subject.S
-    const body = record.dynamodb.NewImage.body.S
+        //pull off items from stream
+        const examLink = record.dynamodb.NewImage.examLink.S
+        const toAddress = record.dynamodb.NewImage.toAddress.S
+        const fromAddress = record.dynamodb.NewImage.fromAddress.S
+        const subject = record.dynamodb.NewImage.subject.S
+        const body = record.dynamodb.NewImage.body.S
 
-      const sendEmailCommand = createSendEmailCommand(
-          toAddress,
-          process.env.SES_EMAIL,
-          body,
-          subject
-      );
+        const sendEmailCommand = createSendEmailCommand(
+            toAddress,
+            process.env.SES_EMAIL,
+            body,
+            subject
+        );
 
 
-    if (record.eventName === 'INSERT') {
-        try {
-            return await sesClient.send(sendEmailCommand);
-        } catch (e) {
-            console.error("Failed to send email.");
-            return e;
+        if (record.eventName === 'INSERT') {
+            try {
+                return await sesClient.send(sendEmailCommand);
+            } catch (e) {
+                console.error("Failed to send email.");
+                return e;
+            }
         }
     }
-  }
 };
