bb98058a64f7f3e5384d0eed3a16c72b3786aae7
Thu Ha Ngo <ngoha241198@gmail.com>
2023-07-06 21:16:38 +0700

    Add cert generating feature

--- src/pages/ReviewSessionPage.js
+++ src/pages/ReviewSessionPage.js
@@ -1,10 +1,13 @@
-import {Heading, View, Text, Flex, Card, Collection} from "@aws-amplify/ui-react";
+import {Heading, View, Text, Flex, Card, Collection, Button} from "@aws-amplify/ui-react";
 import {useParams} from "react-router";
 import {React, useState, useEffect} from "react";
 import {getSessionWithScore} from "../api/scoreApi";
 import {useImmer} from "use-immer";
 import {formatDate} from "../hooks/utils";
 import {FaCheckCircle, FaCross, FaTimes, FaTimesCircle} from "react-icons/fa";
+import { getCert } from "../api/certResource";
+import jsPDF from "jspdf";
+import { callAddFont } from "../hooks/emberFont";
 
 const ReviewSessionPage = () => {
     const [allStates, setAllStates] = useImmer({})
@@ -23,6 +26,28 @@ const ReviewSessionPage = () => {
 
         fetchSessionDataWithScore()
     }, [sessionid])
+
+    const handleDownloadCertificate = () => {
+        // var win = window.open("", "Certificate of Completion", "toolbar=no,location=no,directories=no,status=no,menubar=no,scrollbars=no,resizable=no,width=1024,height=720,top="+(window.screen.height-800)+",left="+(window.screen.width-1024));
+        // win.document.body.innerHTML = getCert(allStates.data.username, formatDate(allStates.Exam.date))
+        // win.document.close();
+        // win.print();
+
+        const content = getCert(allStates.data.username, formatDate(allStates.Exam.date))
+        // jsPDF.API.events.push(['addFonts', callAddFont])
+        let doc = new jsPDF({
+            orientation: 'landscape',
+        })
+        doc.html(content, {
+            callback: function (doc) {
+                doc.save('certificate-of-completion.pdf')
+            },
+            x: 0,
+            y: 0,
+            width: 298.5,
+            windowWidth: 1024,
+        })
+    }
 
     return (
         <Card>
@@ -43,6 +68,11 @@ const ReviewSessionPage = () => {
                         <Card>
                             <Text>Score: {allStates.mark} / {allStates.highestMark}</Text>
                             <Text>Result: {allStates.scorePercentage >= 80 ? "PASSED" : "FAILED"}</Text>
+                            {
+                                allStates.scorePercentage >= 80 && (
+                                    <Button onClick={handleDownloadCertificate}>Download Certificate</Button>
+                                )
+                            }
                         </Card>
                         <Card>
                             <Heading level={4}>Answers</Heading>
