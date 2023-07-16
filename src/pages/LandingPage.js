f96a8007e8a64a52b430e76bb751e78a7bc05f5d
ThuHaNgo11 <21520131@gm.uit.edu.vn>
2023-07-16 10:06:19 +0700

    Update UI for landing page

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,19 +1,30 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 // import Amplify UI components
-import {Heading, View, Card, Flex, Text} from '@aws-amplify/ui-react'
+import {Heading, View, Card, Flex, Text, Button} from '@aws-amplify/ui-react'
 
 // import components
 import LandingPageNavBar from '../components/LandingPageNavBar'
 import NavBar from '../components/NavBar'
 
 import {useAuthenticator} from '@aws-amplify/ui-react'
+import { Navigate } from 'react-router';
+
+import {RxRocket} from "react-icons/rx";
 import "../css/landing-page.css"
 
 const LandingPage = () => {
 
     const {route} = useAuthenticator(context => [context.route])
     const {user, signOut} = useAuthenticator(context => [context.user])
+    const [redirectedPath, setRedirectedPath] = useState("")
+    const redirect = (path) => {
+        return (event) => {
+            event.preventDefault()
+            event.stopPropagation()
+            setRedirectedPath(path)
+        }
+    }
 
     return (
         <View>
@@ -21,39 +32,48 @@ const LandingPage = () => {
                 route === "authenticated" ? <NavBar userName={user.username} signOut={signOut}/> : <LandingPageNavBar/>
             }
             <View>
+                {
+                    !!redirectedPath && <Navigate to={redirectedPath} />
+                }
                 <Flex direction="column" justifyContent="space-between">
                     <Card textAlign="center" class="landing-banner">
                         <Heading level={1} textAlign="center">Welcome AWS builders</Heading>
                         FCJ Exam Platform for creating workshop quizzes
                     </Card>
-                    <Flex direction="row" justifyContent="space-evenly" height="160px" margin="150px 0 0 0">
-                        <Card className="intro-box">
-                            <Flex direction="row" justifyContent="flex-start" gap="1em">
-                                <img width="110px" src={"/assets/img/question.png"} />
-                                <Flex direction="column" width="300px">
-                                    <Heading level={3} textAlign="center">Question Manager</Heading>
+                    <Flex height="calc(100vh - 350px)" direction="column" justifyContent="center" alignItems="stretch" gap="40px">
+                        <Flex direction="row" justifyContent="center" gap="3vw">
+                            <Flex className="intro-box" direction="row" justifyContent="flex-start" gap="1em" wrap="wrap"
+                                  onClick={redirect("/questions")}>
+                                <img width="100px" height="100px" src={"/assets/img/question.png"} />
+                                <Flex direction="column" width="calc(20vw - 130px)" minWidth="120px">
+                                    <Heading level={3} textAlign="center">Question Bank</Heading>
                                     <Text>Manage question bank and create more question contents.</Text>
                                 </Flex>
                             </Flex>
-                        </Card>
-                        <Card className="intro-box">
-                            <Flex direction="row" justifyContent="flex-start" gap="1em">
-                                <img width="110px" src={"/assets/img/test.png"} />
-                                <Flex direction="column" width="300px">
+                            <Flex className="intro-box" direction="row" justifyContent="flex-start" gap="1em" wrap="wrap"
+                                  onClick={redirect("/tests")}>
+                                <img width="100px" height="100px" src={"/assets/img/test.png"} />
+                                <Flex direction="column" width="calc(20vw - 130px)" minWidth="120px">
                                     <Heading level={3} textAlign="center">Test Manager</Heading>
                                     <Text>Pick questions from the question back to compile tests.</Text>
                                 </Flex>
                             </Flex>
-                        </Card>
-                        <Card className="intro-box">
-                            <Flex direction="row" justifyContent="flex-start" gap="1em">
-                                <img width="110px" src={"/assets/img/exam.png"} />
-                                <Flex direction="column" width="300px">
+                            <Flex className="intro-box" direction="row" justifyContent="flex-start" gap="1em" wrap="wrap"
+                                  onClick={redirect("/exams")}>
+                                <img width="100px" height="100px" src={"/assets/img/exam.png"} />
+                                <Flex direction="column" width="calc(20vw - 130px)" minWidth="120px">
                                     <Heading level={3} textAlign="center">Exam Manager</Heading>
                                     <Text>Pick predefined tests and set up exams for test takes.</Text>
                                 </Flex>
                             </Flex>
-                        </Card>
+                        </Flex>
+                        <Flex direction="row" justifyContent="space-evenly">
+                            <Flex direction="column" className="get-started-box" alignItems="center" width="71vw">
+                                <RxRocket size='100' color="white"/>
+                                <Heading level={3}>Learn more to begin creating quizzes for your workshop.</Heading>
+                                <Button className="get-started-button" onClick={redirect('/get-started')}>Get Started</Button>
+                            </Flex>
+                        </Flex>
                     </Flex>
                 </Flex>
             </View>
